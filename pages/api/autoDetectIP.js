a03d9675e290cbce5bde1d127924d8a88b7bf17e
Shashank Jha <shashankjha1994@gmail.com>
2023-08-26 15:38:34 +0530

    First working commit

--- pages/api/autoDetectIP.js
+++ pages/api/autoDetectIP.js
@@ -10,25 +10,30 @@ export default async function handler(req, res) {
     const now = Date.now();
     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 
-    // Implement rate limiting
-    if (!rateLimit.ipCache[ip]) {
-        rateLimit.ipCache[ip] = { count: 0, lastRequest: now };
-    } else {
-        if (now - rateLimit.ipCache[ip].lastRequest < rateLimit.windowMs) {
-            rateLimit.ipCache[ip].count += 1;
+    try {
+        // Implement rate limiting
+        if (!rateLimit.ipCache[ip]) {
+            rateLimit.ipCache[ip] = { count: 0, lastRequest: now };
         } else {
-            rateLimit.ipCache[ip] = { count: 1, lastRequest: now };
+            if (now - rateLimit.ipCache[ip].lastRequest < rateLimit.windowMs) {
+                rateLimit.ipCache[ip].count += 1;
+            } else {
+                rateLimit.ipCache[ip] = { count: 1, lastRequest: now };
+            }
         }
-    }
 
-    if (rateLimit.ipCache[ip].count > rateLimit.max) {
-        return res.status(429).json({ error: "Too many requests" });
-    }
+        if (rateLimit.ipCache[ip].count > rateLimit.max) {
+            return res.status(429).json({ error: "Too many requests" });
+        }
 
-    const data = getGeoData(ip);
-    if (data.error) {
-        return res.status(404).json({ error: data.error });
-    }
+        const data = getGeoData(ip);
+        if (data.error) {
+            return res.status(404).json({ error: data.error });
+        }
 
-    res.status(200).json(data);
+        res.status(200).json(data);
+    } catch (error) {
+        console.error('Internal Server Error:', error);
+        return res.status(500).json({ error: 'Internal Server Error' });
+    }
 }
